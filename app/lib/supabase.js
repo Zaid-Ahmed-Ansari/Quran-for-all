4ff39ab203ff550db941d5aeda7bfb9fe6f39b97
Zaid Ahmed Ansari <ahmedzaid2627@gmail.com>
2025-11-13 17:40:31 +0530

    Updated .env

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const db = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SERVICEE_KEY
 );
